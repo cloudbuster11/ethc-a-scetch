9e372f1c2b5f637d976e8fa09ce96a9901186826
Joakim Jönsson <jockejockejockejocke@gmail.com>
2022-08-30 15:14:01 +0200

    Created addEventlistner function to color clicked div.

--- scripts.js
+++ scripts.js
@@ -5,9 +5,16 @@ function makeRows(rows, cols) {
   container.style.setProperty("--grid-cols", cols);
   for (c = 0; c < rows * cols; c++) {
     let cell = document.createElement("div");
-    cell.innerText = c + 1;
-    container.appendChild(cell).className = "grid-items";
+    // cell.innerText = c + 1;
+    container.appendChild(cell).className = `${c + 1}`;
   }
 }
 
 makeRows(16, 16);
+
+// Colors clicked div.
+container.addEventListener("click", function (e) {
+  if (e.target.matches("div")) {
+    e.target.classList.add("active");
+  }
+});
